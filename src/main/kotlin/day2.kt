22c82bdf3dc01e9080d8cb3fd0c5836a9b2f9be6
Noemi <a49226@alunos.isel.pt>
2022-12-05 22:21:50 +0000

    day2

--- src/main/kotlin/day2.kt
+++ src/main/kotlin/day2.kt
@@ -2,6 +2,7 @@ import java.io.File
 
 val inputLines = File("C:\\ISEL\\AdventOfCode2022\\AdventOfCode2022\\src\\main\\resources\\day2input.txt").readLines()
 
+// Part 1
 fun List<String>.myPoints(): Int {
     var points: List<Int> = emptyList()
     this.forEach {
@@ -18,7 +19,7 @@ fun List<String>.myPoints(): Int {
     this.forEach {
         if (it[2] == 'X') points += 1
         else if (it[2] == 'Y') points += 2
-        else if (it[2] == 'z') points += 3
+        else if (it[2] == 'Z') points += 3
     }
     return points.sumOf { it }
 }
@@ -39,16 +40,47 @@ fun List<String>.elfPoints(): Int {
     this.forEach {
         if (it[0] == 'X') points += 1
         else if (it[0] == 'Y') points += 2
-        else if (it[0] == 'z') points += 3
+        else if (it[0] == 'Z') points += 3
     }
     return points.sumOf { it }
 }
 
+// Part 2
+
+fun List<String>.updatedMyPoints(): Int {
+    var points: List<Int> = emptyList()
+    this.forEach {
+        if (it[2] == 'X') points += 0
+        else if (it[2] == 'Y') points += 3
+        else if (it[2] == 'Z') points += 6
+    }
+    this.forEach {
+        if ((it[0] == 'B' && it[2] == 'X')
+            || (it[0] == 'A' && it[2] == 'Y')
+            || (it[0] == 'C' && it[2] == 'Z')) points += 1 //Rock
+        else if ((it[0] == 'A' && it[2] == 'Z')
+            || (it[0] == 'C' && it[2] == 'X')
+            || (it[0] == 'B' && it[2] == 'Y')) points += 2 //Paper
+        else if ((it[0] == 'C' && it[2] == 'Y')
+            || (it[0] == 'B' && it[2] == 'Z')
+            || (it[0] == 'A' && it[2] == 'X')) points += 3 //Scissors
+    }
+    return points.sumOf { it }
+}
+
+
 fun main() {
+    println("***** PART ONE *****")
     println("My score is ${inputLines.myPoints()}")
     println("The elf's score is ${inputLines.elfPoints()}")
     println(if (inputLines.myPoints() > inputLines.elfPoints()) "I win!"
             else if (inputLines.elfPoints() > inputLines.myPoints()) "The elf wins..."
             else "It's a draw!")
-
+    println()
+    println("***** PART TWO *****")
+    println("My updated score is ${inputLines.updatedMyPoints()}")
+    println("The elf's score is ${inputLines.elfPoints()}")
+    println(if (inputLines.updatedMyPoints() > inputLines.elfPoints()) "I win!"
+    else if (inputLines.elfPoints() > inputLines.updatedMyPoints()) "The elf wins..."
+    else "It's a draw!")
 }
