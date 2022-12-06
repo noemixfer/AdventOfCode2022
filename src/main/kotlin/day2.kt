747a6949b929017508427ed2ba89d56c7841bbb4
Noemi <a49226@alunos.isel.pt>
2022-12-06 10:03:57 +0000

    day2

--- src/main/kotlin/day2.kt
+++ src/main/kotlin/day2.kt
@@ -4,68 +4,68 @@ val inputLines = File("C:\\ISEL\\AdventOfCode2022\\AdventOfCode2022\\src\\main\\
 
 // Part 1
 fun List<String>.myPoints(): Int {
-    var points: List<Int> = emptyList()
+    var points: Int = 0
     this.forEach {
         if ((it[0] == 'A' && it[2] == 'X')
             || (it[0] == 'B' && it[2] == 'Y')
-            || (it[0] == 'C' && it[2] == 'Z')) points += 3
+            || (it[0] == 'C' && it[2] == 'Z')) points += 3 // Draw
         else if ((it[0] == 'A' && it[2] == 'Z')
             || (it[0] == 'B' && it[2] == 'X')
-            || (it[0] == 'C' && it[2] == 'Y')) points += 0
+            || (it[0] == 'C' && it[2] == 'Y')) points += 0 // Lose
         else if ((it[0] == 'A' && it[2] == 'Y')
             || (it[0] == 'B' && it[2] == 'Z')
-            || (it[0] == 'C' && it[2] == 'X')) points += 6
+            || (it[0] == 'C' && it[2] == 'X')) points += 6 // Win
     }
     this.forEach {
-        if (it[2] == 'X') points += 1
-        else if (it[2] == 'Y') points += 2
-        else if (it[2] == 'Z') points += 3
+        if (it[2] == 'X') points += 1 // Rock
+        else if (it[2] == 'Y') points += 2 // Paper
+        else if (it[2] == 'Z') points += 3 // Scissors
     }
-    return points.sumOf { it }
+    return points
 }
 
 fun List<String>.elfPoints(): Int {
-    var points: List<Int> = emptyList()
+    var points: Int = 0
     this.forEach {
         if ((it[0] == 'A' && it[2] == 'X')
             || (it[0] == 'B' && it[2] == 'Y')
-            || (it[0] == 'C' && it[2] == 'Z')) points += 3
+            || (it[0] == 'C' && it[2] == 'Z')) points += 3 // Draw
         else if ((it[0] == 'A' && it[2] == 'Z')
             || (it[0] == 'B' && it[2] == 'X')
-            || (it[0] == 'C' && it[2] == 'Y')) points += 6
+            || (it[0] == 'C' && it[2] == 'Y')) points += 6 // Win
         else if ((it[0] == 'A' && it[2] == 'Y')
             || (it[0] == 'B' && it[2] == 'Z')
-            || (it[0] == 'C' && it[2] == 'X')) points += 0
+            || (it[0] == 'C' && it[2] == 'X')) points += 0 // Lose
     }
     this.forEach {
-        if (it[0] == 'X') points += 1
-        else if (it[0] == 'Y') points += 2
-        else if (it[0] == 'Z') points += 3
+        if (it[0] == 'X') points += 1 // Rock
+        else if (it[0] == 'Y') points += 2 // Paper
+        else if (it[0] == 'Z') points += 3 // Scissors
     }
-    return points.sumOf { it }
+    return points
 }
 
 // Part 2
 
 fun List<String>.updatedMyPoints(): Int {
-    var points: List<Int> = emptyList()
+    var points: Int = 0
     this.forEach {
-        if (it[2] == 'X') points += 0
-        else if (it[2] == 'Y') points += 3
-        else if (it[2] == 'Z') points += 6
+        if (it[2] == 'X') points += 0 // Lose
+        else if (it[2] == 'Y') points += 3 // Draw
+        else if (it[2] == 'Z') points += 6 // Win
     }
     this.forEach {
         if ((it[0] == 'B' && it[2] == 'X')
             || (it[0] == 'A' && it[2] == 'Y')
-            || (it[0] == 'C' && it[2] == 'Z')) points += 1 //Rock
+            || (it[0] == 'C' && it[2] == 'Z')) points += 1 // Rock
         else if ((it[0] == 'A' && it[2] == 'Z')
             || (it[0] == 'C' && it[2] == 'X')
-            || (it[0] == 'B' && it[2] == 'Y')) points += 2 //Paper
+            || (it[0] == 'B' && it[2] == 'Y')) points += 2 // Paper
         else if ((it[0] == 'C' && it[2] == 'Y')
             || (it[0] == 'B' && it[2] == 'Z')
-            || (it[0] == 'A' && it[2] == 'X')) points += 3 //Scissors
+            || (it[0] == 'A' && it[2] == 'X')) points += 3 // Scissors
     }
-    return points.sumOf { it }
+    return points
 }
 
 
@@ -78,6 +78,7 @@ fun main() {
             else "It's a draw!")
 
     println()
+
     println("***** PART TWO *****")
     println("My updated score is ${inputLines.updatedMyPoints()}")
     println("The elf's score is ${inputLines.elfPoints()}")
